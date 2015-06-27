b00797ec1bc42832d0c886c3f1a39332c4572cac
Belshina <Belshina@Asutp-pc>
2015-06-27 09:11:21 +0300

    level11.lesson11.bonus03;
    /* ?????? ?? ??????????
    ???????? ?????, ??????? ?????????? ??????????? ? ???????????? ????? ? ???????.
    */

--- src/com/javarush/test/level11/lesson11/bonus03/Solution.java
+++ src/com/javarush/test/level11/lesson11/bonus03/Solution.java
@@ -34,7 +34,17 @@ public class Solution
             if (j > max)
                 max = j;
         }
-
+/**
+//===============================================
+       int min = array[0];
+       int max = array[0];
+       for (int i = 0; i < array.length; i++)
+       {
+           if (array[i] < min) min = array[i];
+           if (array[i] > max) max = array[i];
+       }
+//=========================================================
+*/
 
         return new Pair<Integer, Integer>(min, max);
     }
