be77acb84451f92adfe32215bde608262091600c
Belshina <Belshina@Asutp-pc>
2015-06-27 07:48:41 +0300

    level11.lesson11.home06;
    /* ?????? ?????????? ???????? ?????????????
    ???????? ????????? ???????? ????????????? ? ???????: Pet (???????? ????????), Cat (???),
    Dog(??????).
    */

---
+++ src/com/javarush/test/level11/lesson11/home06/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.test.level11.lesson11.home06;
+
+/* Первая правильная «цепочка наследования»
+Расставь правильно «цепочку наследования» в классах: Pet (домашнее животное), Cat (кот),
+Dog(собака).
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+    }
+
+    public class Pet
+    {
+
+    }
+
+    public class Cat extends Pet
+    {
+
+    }
+
+    public class Dog extends Pet
+    {
+
+    }
+}
+
+/**
+public class Solution
+{
+    public static void main(String[] args)
+    {
+    }
+
+    public class Pet
+    {
+
+    }
+
+    public class Cat
+    {
+
+    }
+
+    public class Dog
+    {
+
+    }
+}
+*/
