1b2db96a4353b2c874fcf336a7bc4b661790a84a
Belshina <Belshina@Asutp-pc>
2015-06-29 08:30:33 +0300

    level12.lesson06.task01;
    /* ??????????? ????? Pet
    ??????? ????? Pet ???????????.
    */

---
+++ src/com/javarush/test/level12/lesson06/task01/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.test.level12.lesson06.task01;
+
+/* Абстрактный класс Pet
+Сделать класс Pet абстрактным.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public abstract class Pet
+    {
+        public abstract String getName();
+    }
+
+}
+
+/**
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public static class Pet
+    {
+        public String getName()
+        {
+            return "Я - котенок";
+        }
+    }
+
+}
+*/
