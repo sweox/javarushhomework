92018fb194a602c4f44cefa0087aac2660bad5c0
Belshina <Belshina@Asutp-pc>
2015-07-02 14:30:45 +0300

    level13.lesson02.task02;
    /* ????: ???????????
    ?????? ? ?????? Beer ????????? Drink ? ???????? ??? ??????????????? ??????.
    */

--- src/com/javarush/test/level13/lesson02/task02/Solution.java
+++ src/com/javarush/test/level13/lesson02/task02/Solution.java
@@ -37,3 +37,35 @@ public class Solution
 
     }
 }
+
+/**
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        Drink beer = new Beer();
+        System.out.println(beer.toString());
+    }
+
+    public interface Drink
+    {
+        boolean isAlcoholic();
+    }
+
+    public static class Beer implements Drink
+    {
+        @Override
+        public String toString()
+        {
+            if (isAlcoholic()) {
+                return "Напиток алкогольный";
+            }
+            else {
+                return "Напиток безалкогольный";
+            }
+
+        }
+
+    }
+}
+*/
