55712863c1b0efbb73324a7ebe34cc28679f2164
Belshina <Belshina@Asutp-pc>
2015-07-03 17:09:00 +0300

    level13.lesson02.task07;
    /* ???????????????? ?????????
    ? ?????? StringObject ???????? ????????? SimpleObject ? ?????????? ???? String.
    */

--- src/com/javarush/test/level13/lesson02/task07/Solution.java
+++ src/com/javarush/test/level13/lesson02/task07/Solution.java
@@ -25,3 +25,21 @@ public class Solution
         }
     }
 }
+
+/**
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+    }
+
+    interface SimpleObject<T>
+    {
+        SimpleObject<T> getInstance();
+    }
+
+    class StringObject //допишите здесь ваш код
+    {
+    }
+}
+*/
