cfbe29e9216e92fccd0fdc3ca241c247e4899b95
Admin <Admin@4530s>
2015-07-04 02:18:35 +0300

    level13.lesson11.home02;
    /* ??????
    1. ? ?????? Today ??????????? ????????? Weather.
    2. ???????, ??? ?????? ???????? String type ? ??????? getWeatherType().
    3. ?????????? Weather ? WeatherType ??? ???????????.
    */

--- src/com/javarush/test/level13/lesson11/home02/Solution.java
+++ src/com/javarush/test/level13/lesson11/home02/Solution.java
@@ -31,7 +31,7 @@ public class Solution
         @Override
         public String toString()
         {
-            return String.format("%s for today", this.getWeatherType());
+            return String.format("%s for today",  this.getWeatherType());
         }
     }
 }
