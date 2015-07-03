2c91afc8c1080c17b3e144e25b78b5a4e5629bdc
Belshina <Belshina@Asutp-pc>
2015-07-03 16:56:14 +0300

    level13.lesson02.task07;
    /* ???????????????? ?????????
    ? ?????? StringObject ???????? ????????? SimpleObject ? ?????????? ???? String.
    */

---
+++ src/com/javarush/test/level13/lesson02/task07/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.test.level13.lesson02.task07;
+
+/* Параметризованый интерфейс
+В классе StringObject реализуй интерфейс SimpleObject с параметром типа String.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+    }
+
+    interface SimpleObject<T>
+    {
+        SimpleObject<T> getInstance();
+    }
+
+    class StringObject implements SimpleObject<String>//допишите здесь ваш код
+    {
+        private SimpleObject<String> t;
+        public SimpleObject<String> getInstance()
+        {
+            return t;
+        }
+    }
+}
