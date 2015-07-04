42ec5afe61431c84ff721cc3e792764a95fcccaa
Belshina <Belshina@Asutp-pc>
2015-07-04 19:41:35 +0300

    level13.lesson11.home03;
    /* ?????? ?????
    1. ??????? ? ??????? ??? ?????.
    2. ??????? ? ???????(?? ?????) ?????????? ?????.
    3. ?? ?????? ??????? ???? ? ?????.
    */

---
+++ src/com/javarush/test/level13/lesson11/home03/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.test.level13.lesson11.home03;
+
+/* Чтение файла
+1. Считать с консоли имя файла.
+2. Вывести в консоль(на экран) содержимое файла.
+3. Не забыть закрыть файл и поток.
+*/
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+
+public class Solution
+{
+    public static void main(String[] args) throws IOException
+    {
+        //add your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String path = reader.readLine();
+
+        FileInputStream input = new FileInputStream(path);
+        ArrayList<Integer> listInteger = new ArrayList<Integer>();
+        ArrayList<String> listString = new ArrayList<String>();
+        //FileOutputStream output = new FileOutputStream();
+        while (input.available() > 0)
+        {
+            int data = input.read();
+            listInteger.add(data);
+        }
+        for (int f:listInteger)
+        {
+            String str = Integer.toString(f);
+            listString.add(str);
+        }
+        input.close();
+        System.out.println(listString);
+    }
+}
