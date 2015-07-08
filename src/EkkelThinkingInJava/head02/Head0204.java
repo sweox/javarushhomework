399742feb5ff578b85881e3c4140e9eda13029a3
Belshina <Belshina@Asutp-pc>
2015-07-09 02:06:10 +0300

    EkkelThinkingInJava.head02

---
+++ src/EkkelThinkingInJava/head02/Head0204.java
@@ -0,0 +1,14 @@
+package EkkelThinkingInJava.head02;
+
+/**
+ * Created by Belshina on 09.07.15.
+ */
+public class Head0204
+{
+    class DataOnly{
+        int i;
+        double d;
+        boolean b;
+        DataOnly data = new DataOnly();
+    }
+}
