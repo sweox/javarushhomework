0dedc94c8e3ba26c956f45aa27d4eef85c72533d
Belshina <Belshina@Asutp-pc>
2015-09-03 19:32:07 +0300

    com.javarush.test.level13.lesson11.home08

--- src/com/javarush/test/level13/lesson11/home08/Solution.java
+++ src/com/javarush/test/level13/lesson11/home08/Solution.java
@@ -25,3 +25,27 @@ public class Solution
         public void refresh() {}
     }
 }
+
+/*
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+    }
+
+    interface Selectable
+    {
+        void onSelect();
+    }
+
+    interface Updatable extends Selectable
+    {
+        void refresh();
+    }
+
+    class Screen implements Updatable
+    {
+
+    }
+}
+*/
