573aa9802c7fc7b034fe6d9955ff41220d7db779
Admin <Admin@4530s>
2015-09-14 21:21:30 +0300

    com.javarush.test.level14.lesson04.task04;
    /* ??? ??????
    ???????????????? ?????? obj ????? ???????, ????? ????? main ?????????? ??? ??????.
    */

---
+++ src/com/javarush/test/level14/lesson04/task04/Solution.java
@@ -0,0 +1,80 @@
+package com.javarush.test.level14.lesson04.task04;
+
+/* Без ошибок
+Инициализировать объект obj таким классом, чтобы метод main выполнился без ошибок.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Object obj = new Jerry();//Add your code here
+
+        Mouse mouse = (Mouse) obj;
+        GreyMouse greyMouse = (GreyMouse) mouse;
+        Jerry jerry = (Jerry) greyMouse;
+
+        printClasses(obj, mouse, greyMouse, jerry);
+
+    }
+
+    public static void printClasses(Object obj, Mouse mouse, GreyMouse greyMouse, Jerry jerry)
+    {
+        System.out.println(jerry.getClass().getSimpleName());
+        System.out.println(greyMouse.getClass().getSimpleName());
+        System.out.println(mouse.getClass().getSimpleName());
+        System.out.println(obj.getClass().getSimpleName());
+    }
+
+    static class Mouse
+    {
+    }
+
+    static class GreyMouse extends Mouse
+    {
+    }
+
+    static class Jerry extends GreyMouse
+    {
+    }
+}
+/*
+Потому, что на самом деле у вас один объект, а то его приводят к Object или любому его предку
+        сути не меняет - объект то принадлежит к классу Jerry.
+*/
+/*
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Object obj = //Add your code here
+
+        Mouse mouse = (Mouse) obj;
+        GreyMouse greyMouse = (GreyMouse) mouse;
+        Jerry jerry = (Jerry) greyMouse;
+
+        printClasses(obj, mouse, greyMouse, jerry);
+
+    }
+
+    public static void printClasses(Object obj, Mouse mouse, GreyMouse greyMouse, Jerry jerry)
+    {
+        System.out.println(jerry.getClass().getSimpleName());
+        System.out.println(greyMouse.getClass().getSimpleName());
+        System.out.println(mouse.getClass().getSimpleName());
+        System.out.println(obj.getClass().getSimpleName());
+    }
+
+    static class Mouse
+    {
+    }
+
+    static class GreyMouse extends Mouse
+    {
+    }
+
+    static class Jerry extends GreyMouse
+    {
+    }
+}
+*/
