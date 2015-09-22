8ab0291ca40505e1a1c6f95657220e9f81aea8e3
Belshina <Belshina@Asutp-pc>
2015-09-22 16:00:23 +0200

    com.javarush.test.level14.lesson08.home05;
    /* Computer
    1. ?????? ????????? CompItem.
    2. ?????? ? ???? ????? String getName().
    3. ?????? ?????? Keyboard, Mouse, Monitor, ??????? ????????? ????????? CompItem.
    4. ????? getName() ?????? ?????????? ??? ??????, ????????, ??? ?????? Keyboard ????? "Keyboard".
    5. ?????? ????? Computer.
    6. ? ????? Computer ?????? ????????? ???? ???? Keyboard.
    7. ? ????? Computer ?????? ????????? ???? ???? Mouse.
    8. ? ????? Computer ?????? ????????? ???? ???? Monitor.
    9. ?????? ??????????? ? ?????? Computer ????????? ?????????? ?????? Alt+Insert ?????? ??????
    (??????? Constructor).
    10 ?????? ???????????? ??????????????? ??? ??? ???? (??????????) ??????
    11. ?????? ??????? ??? ????? ?????? Computer (? ?????? ????????? ?????????? ?????? Alt+Insert
    ? ?????? ??????? Getter).
    12. ??? ????????? ?????? ? ????????? ?????? ???? ? ????????? ??????.
    13. ????? Solution ?????? ??????.
    */

---
+++ src/com/javarush/test/level14/lesson08/home05/CompItem.java
@@ -0,0 +1,9 @@
+package com.javarush.test.level14.lesson08.home05;
+
+/**
+ * Created by Belshina on 22.09.15.
+ */
+public interface CompItem
+{
+    String getName();
+}
---
+++ src/com/javarush/test/level14/lesson08/home05/Mouse.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level14.lesson08.home05;
+
+/**
+ * Created by Belshina on 22.09.15.
+ */
+public class Mouse implements CompItem
+{
+    public String getName()
+    {
+        return "Mouse";
+    }
+}
