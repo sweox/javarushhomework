83b45f19210a3348836bd091b5e7791cf3ad706f
Admin <Admin@4530s>
2015-09-23 21:17:08 +0300

    com.javarush.test.level14.lesson08.home09;
    /* ??????
    1. ???????? ????? getAmount ? ?????? Money:
    1.1. ???????, ?????? ???? ????? ??????? ????????? ??????????, ???? ????? getAmount ????? ?? ??????????.
    1.2. ?????? ????????? ?????????? ????? ???? ? ????? ?? ? ?????? getAmount.
    1.3. ? ???????????? ??????? ?? ????????, ?????????? ??????????.
    2. ? ????????? ????? ?????? ????? Hrivna.
    3. ???????? ????? Hrivna ?? ?????? Money.
    4. ? ?????? Hrivna ???????? ????? getCurrencyName, ??????? ?????????? "HRN".
    5. ? ????????? ????? ?????? ????? USD.
    6. ???????? ????? USD ?? ?????? Money.
    7. ? ?????? USD ???????? ????? getCurrencyName, ??????? ?????????? "USD".
    8. ???????, ??????? ????? ??????? ????? ???????? ? ??????(????) allMoney.
    9. ?????? ? ??????????? ?????? Person ?????????? ????? allMoney ????????? ???? ????????? ???????.
    */

---
+++ src/com/javarush/test/level14/lesson08/home09/Money.java
@@ -0,0 +1,27 @@
+package com.javarush.test.level14.lesson08.home09;
+
+public abstract class Money
+{
+    public Money(double amount)
+    {
+    }
+
+    public abstract double getAmount();
+
+    public abstract String getCurrencyName();
+}
+
+
+/*
+public abstract class Money
+{
+    public Money(double amount)
+    {
+    }
+
+    public abstract double getAmount();
+
+    public abstract String getCurrencyName();
+}
+
+*/
