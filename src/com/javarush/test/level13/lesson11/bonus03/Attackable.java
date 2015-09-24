44d03b9a9e431d7f0d4c3be90f6c7f5181212330
admin <admin@server>
2015-09-24 15:05:37 +0300

    com.javarush.test.level13.lesson11.bonus03;
    /* ????? ???????
    1 ??????????? ? ???, ??? ??? ????????.
    2 http://info.javarush.ru/uploads/images/00/00/07/2014/08/12/50f3e37f94.png
    3 ????????? ?? ????? ??????? ? ?????????? ??????????? ? ????.
    4 ...
    5 ????????????, ??? ?? ??? ??????.
    
    6 ???????? ??? ???????? ????? ??????????? ? ???????? ????? ??????:
    6.1 ??????? ????? AbstractRobot ???????????, ??????? ?????? ????? ? ?????? ?? Robot ? AbstractRobot.
    6.2 ??????????????? ????? Robot ???????? AbstractRobot.
    6.3 ????????? ????? BodyPart ????? ?????? ???? "?????".
    6.4 ???????? ????? ????? ???? ? ?????????? ??????????? Attackable ? Defensable (? ?????? AbstractRobot).
    
    7 http://info.javarush.ru/uploads/images/00/00/07/2014/08/12/3b9c65580b.png
    */

---
+++ src/com/javarush/test/level13/lesson11/bonus03/Attackable.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level13.lesson11.bonus03;
+
+public interface Attackable
+{
+    BodyPart attack();
+}
+
+
+/*
+public interface Attackable
+{
+    BodyPart attack();
+}
+*/
---
+++ src/com/javarush/test/level13/lesson11/bonus03/Defensable.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level13.lesson11.bonus03;
+
+public interface Defensable
+{
+    BodyPart defense();
+}
+
+
+/*
+public interface Defensable
+{
+    BodyPart defense();
+}
+*/
