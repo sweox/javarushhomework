037c1715f153e3687419acf12760bffe5acbc704
Belshina <Belshina@Asutp-pc>
2015-09-25 02:14:13 +0200

    com.javarush.test.level15.lesson02.task03;
    /* ??? - ???????
    1. ??? ?????? ???????????? ????? ?????? ?????????? ?? ????? Constants.
    2. ? ?????? Ferrari ?????????? ????? printlnDesire, ????? ?? ??????? ?? ????? "? ???? ??????
    ?? ???????".
    3. ? ?????? Lanos ?????????? ????? printlnDesire, ????? ?? ??????? ?? ????? "? ???? ??????
    ?? ??????".
    4. ???????? public static ????? LuxuriousCar(???????????????).
    5. ???????? public static ????? CheapCar(?????????????).
    6. ??????????? Ferrari ? Lanos ?? CheapCar ? LuxuriousCar, ?????????, ????? ????? ??? ????.
    7. ? ?????? LuxuriousCar ?????????? ????? printlnDesire, ????? ?? ??????? ?? ????? "? ????
    ?????? ?? ????????? ??????".
    8. ? ?????? CheapCar ?????????? ????? printlnDesire, ????? ?? ??????? ?? ????? "? ???? ??????
    ?? ??????? ??????".
    9. ? ?????? LuxuriousCar ? CheapCar ??? ?????? printlnDesire ?????????? ?????????? ?????????
    ???????????? ??????? ???,????? ? ??????? Ferrari ? Lanos ??????????? ?????????? ?????????.
    */

--- src/com/javarush/test/level15/lesson02/task03/Solution.java
+++ src/com/javarush/test/level15/lesson02/task03/Solution.java
@@ -14,10 +14,51 @@ package com.javarush.test.level15.lesson02.task03;
 8. В классе CheapCar реализуйте метод printlnDesire, чтобы он выводил на экран "Я хочу ездить
 на дешевой машине".
 9. В класах LuxuriousCar и CheapCar для метода printlnDesire расставьте различными способами
-модификаторы доступа так,
-чтобы в классах Ferrari и Lanos выполнялось расширение видимости.
+модификаторы доступа так,чтобы в классах Ferrari и Lanos выполнялось расширение видимости.
 */
 
+public class Solution {
+    public static void main(String[] args) {
+        new Solution.LuxuriousCar().printlnDesire();
+        new Solution.CheapCar().printlnDesire();
+        new Solution.Ferrari().printlnDesire();
+        new Solution.Lanos().printlnDesire();
+    }
+
+    public static class Ferrari extends LuxuriousCar {
+        public void printlnDesire() {
+            //add your code here
+            System.out.println(Constants.WANT_STRING + Constants.FERRARY_NAME);
+        }
+    }
+
+    public static class Lanos extends CheapCar{
+        public void printlnDesire() {
+            //add your code here
+            System.out.println(Constants.WANT_STRING + Constants.LANOS_NAME);
+        }
+    }
+    public static class LuxuriousCar {
+         protected void printlnDesire() {
+            System.out.println(Constants.WANT_STRING + Constants.LUXURIOUS_CAR);
+        }
+    }
+    public static class CheapCar {
+         void printlnDesire() {
+            System.out.println(Constants.WANT_STRING + Constants.CHEAP_CAR);
+        }
+    }
+
+    public static class Constants {
+        public static String WANT_STRING = "Я хочу ездить на ";
+        public static String LUXURIOUS_CAR = "роскошной машине";
+        public static String CHEAP_CAR = "дешевой машине";
+        public static String FERRARY_NAME = "Феррари";
+        public static String LANOS_NAME = "Ланосе";
+    }
+}
+
+/*
 public class Solution {
     public static void main(String[] args) {
         new Solution.LuxuriousCar().printlnDesire();
@@ -46,3 +87,4 @@ public class Solution {
         public static String LANOS_NAME = "Ланосе";
     }
 }
+*/
