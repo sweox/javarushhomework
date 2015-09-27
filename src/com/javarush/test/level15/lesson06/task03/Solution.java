956ec8ea3562a55b06bb42e4d04c9859728d16c2
Belshina <Belshina@Asutp-pc>
2015-09-28 01:54:53 +0200

    com.javarush.test.level15.lesson06.task03;
    /* ??????????? ??????? ??? 3
    ??????? ???:
    - ????? ??? ??, ??? ????? ?????????????? ????????????? ??? ??????????
    - ????? ??? ???????????? ???????, ??????? ? ??? ????? ????????? ????????????? ??? ??????????
    - ????? ??? ????????????, ??????? ????????? ? ??????????? ?????????????.
    */

--- src/com/javarush/test/level15/lesson06/task03/Solution.java
+++ src/com/javarush/test/level15/lesson06/task03/Solution.java
@@ -23,3 +23,27 @@ public class Solution {
 
     }
 }
+
+
+/*
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public interface Runnable {
+
+    }
+
+    public class Machine extends Object implements Runnable {
+        public Machine() {
+            super();
+        }
+    }
+
+    public class Car extends Machine implements Runnable {
+        public Car() {
+            super();
+        }
+    }
+}
+*/
