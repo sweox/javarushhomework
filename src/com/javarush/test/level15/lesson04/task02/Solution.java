d57a15d7e8a1a818cc4eacdcdacb6ad778b9281e
admin <admin@server>
2015-09-27 21:47:14 +0300

    com.javarush.test.level15.lesson04.task02;
    /* ??? - ??????????
    ??????????? ????? printMatrix 8 ?????????? ?????????. ? ????? ?????? ?????????? 10 ????????? ??????? printMatrix.
    */

---
+++ src/com/javarush/test/level15/lesson04/task02/Solution.java
@@ -0,0 +1,122 @@
+package com.javarush.test.level15.lesson04.task02;
+
+/* ООП - Перегрузка
+Перегрузите метод printMatrix 8 различными способами. В итоге должно получиться 10 различных методов printMatrix.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        printMatrix(2, 3, "8");
+    }
+//-----------1-------------
+    public static void printMatrix(int m, int n, String value) {
+        System.out.println("Заполняем объектами String");
+        printMatrix(m, n, (Object) value);
+    }
+//-----------2--------------
+    public static void printMatrix(int m, int n, Object value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+    //---------3---------------
+    public static void printMatrix(Integer m, Integer n, String value){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+    //---------------4-----------------
+    public static void printMatrix(int m, Integer n, String value){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+//---------------5-----------------
+    public static void printMatrix(Integer m, int n, String value){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+//---------------6-----------------
+    public static void printMatrix(double m, Integer n, String value){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+//---------------7-----------------
+    public static void printMatrix(int m, double n, String value){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+//---------------8-----------------
+    public static void printMatrix(int m, Integer n, Object value){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+//---------------9-----------------
+    public static void printMatrix(float m, Integer n, String value){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+//---------------10-----------------
+    public static void printMatrix(int m, char n, String value){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+
+}
+
+
+/*
+
+public class Solution {
+    public static void main(String[] args) {
+        printMatrix(2, 3, "8");
+    }
+
+    public static void printMatrix(int m, int n, String value) {
+        System.out.println("Заполняем объектами String");
+        printMatrix(m, n, (Object) value);
+    }
+
+    public static void printMatrix(int m, int n, Object value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+}
+*/
