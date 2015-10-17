acb6a7416a8bd04ff8e45049c2963bcc7c6aa3e2
admin <admin@server>
2015-10-17 19:42:34 +0300

    com.javarush.test.level15.lesson12.home07;
    /* ???? ? ??????????? ?????
    1. ????????????? ????????? Constants.FILE_NAME ?????? ????? ? ????? ? ???????, ??????? ???????? ?????????
    ?????.
    2. ? ??????????? ????? ?????? ?? ????? ? ?????? Constants.FILE_NAME ??? ?????? ? ?????? ?? ??-???????????
    ? List lines.
    3. ?????? ????? ????? ??????? close().
    */

---
+++ src/com/javarush/test/level15/lesson12/home07/Constants.java
@@ -0,0 +1,10 @@
+package com.javarush.test.level15.lesson12.home07;
+
+public class Constants {
+    public static String FILE_NAME = "C:\\1.txt";   /* add your path to source file here*/
+}
+
+//
+//public class Constants {
+//    public static String FILE_NAME = /* add your path to source file here*/;
+//}
