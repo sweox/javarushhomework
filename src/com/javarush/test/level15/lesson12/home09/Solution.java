602f8bccdd0126d6fe8ca0404f9380e7135024ac
Belshina <Belshina@Asutp-pc>
2015-10-18 14:20:58 +0200

    com.javarush.test.level15.lesson12.home09;
    /* ?????? ?????????
    ??????? ? ??????? URl ??????.
    ??????? ?? ????? ????? ?????? ?????? ???? ?????????? (????????? ???? ????? ? ?
    ??????????? &, ????????, lvl=15).
    URL ???????? ??????? 1 ????????.
    ???? ???????????? ???????? obj, ?? ???????? ??? ???????? ? ?????? ????? alert.
    alert(double value) - ??? ????? (??????? ????? ??????????? ??????)
    alert(String value) - ??? ?????
    
    ?????? 1
    ????:
    http://javarush.ru/alpha/index.html?lvl=15&view&name=Amigo
    ?????:
    lvl view name
    
    ?????? 2
    ????:
    http://javarush.ru/alpha/index.html?obj=3.14&name=Amigo
    ?????:
    obj name
    double 3.14

---
+++ src/com/javarush/test/level15/lesson12/home09/Solution.java
@@ -0,0 +1,73 @@
+package com.javarush.test.level15.lesson12.home09;
+
+/* Парсер реквестов
+Считать с консоли URl ссылку.
+Вывести на экран через пробел список всех параметров (Параметры идут после ? и
+разделяются &, например, lvl=15).
+URL содержит минимум 1 параметр.
+Если присутствует параметр obj, то передать его значение в нужный метод alert.
+alert(double value) - для чисел (дробные числа разделяются точкой)
+alert(String value) - для строк
+
+Пример 1
+Ввод:
+http://javarush.ru/alpha/index.html?lvl=15&view&name=Amigo
+Вывод:
+lvl view name
+
+Пример 2
+Ввод:
+http://javarush.ru/alpha/index.html?obj=3.14&name=Amigo
+Вывод:
+obj name
+double 3.14
+*/
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException, ArrayIndexOutOfBoundsException {
+        //add your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String r = reader.readLine();
+        char[] ch = r.toCharArray();
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i]=='?' || ch[i] == '&')
+            {
+                while ((i<ch.length - 1) && !(ch[i+1] == '&'))
+                {
+                    i++;
+                    System.out.print(ch[i]);
+                }
+                System.out.print(" ");
+            }
+        }
+    }
+
+    public static void alert(double value) {
+        System.out.println("double " + value);
+    }
+
+    public static void alert(String value) {
+        System.out.println("String " + value);
+    }
+}
+
+/*
+public class Solution {
+    public static void main(String[] args) {
+        //add your code here
+    }
+
+    public static void alert(double value) {
+        System.out.println("double " + value);
+    }
+
+    public static void alert(String value) {
+        System.out.println("String " + value);
+    }
+}
+*/
