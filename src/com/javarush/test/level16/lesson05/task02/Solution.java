90668c657183e8aa8d570e256a72942c5b1bab71
Belshina <Belshina@Asutp-pc>
2015-10-27 01:50:13 +0200

    com.javarush.test.level16.lesson05.task02;
    /* Horse Racing
    ?????????, ??? ?????? ?????????.
    ???????? ????? calculateHorsesFinished. ?? ??????:
    1. ????????? ?????????? ?????????????? ??????? ? ?????????? ???. ????????? ????? isFinished().
    2. ???? ?????? ??? ?? ?????? ? ?????? (!isFinished()), ??:
    2.1. ??????? ? ??????? "Waiting for " + horse.getName().
    2.2. ?????????, ???? ??? ???????? ?????. ???????, ????? ????? ????? ???????????? ??? ?????.
    */
    /*
    http://habrahabr.ru/post/164487/
    https://ru.wikipedia.org/wiki/%D0%9F%D0%BE%D1%82%D0%BE%D0%BA_%D0%B2%D1%8B%D0%BF%D0%BE%D0%BB%D0%BD%D0%B5%D0%BD%D0%B8%D1%8F
     */

---
+++ src/com/javarush/test/level16/lesson05/task02/Solution.java
@@ -0,0 +1,89 @@
+package com.javarush.test.level16.lesson05.task02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* Horse Racing
+Разберись, что делает программа.
+Реализуй метод calculateHorsesFinished. Он должен:
+1. Посчитать количество финишировавших лошадей и возвратить его. Используй метод isFinished().
+2. Если лошадь еще не пришла к финишу (!isFinished()), то:
+2.1. Вывести в консоль "Waiting for " + horse.getName().
+2.2. Подождать, пока она завершит гонку. Подумай, какой метод нужно использовать для этого.
+*/
+/*
+http://habrahabr.ru/post/164487/
+https://ru.wikipedia.org/wiki/%D0%9F%D0%BE%D1%82%D0%BE%D0%BA_%D0%B2%D1%8B%D0%BF%D0%BE%D0%BB%D0%BD%D0%B5%D0%BD%D0%B8%D1%8F
+ */
+public class Solution {
+    public static int countHorses = 10;
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Horse> horses = prepareHorsesAndStart();
+        while (calculateHorsesFinished(horses) != countHorses) {
+        }
+    }
+
+    public static int calculateHorsesFinished(List<Horse> horses) throws InterruptedException {
+        int countFinished = 0;
+        //add your implementation here - добавь свою реалзацию тут
+/*        for (Horse x:horses) {
+            if (x.isFinished()) {
+                countFinished++;
+            }
+            else if(!x.isFinished()) {
+                System.out.println("Waiting for " + x.getName());
+                x.join();
+            }
+        }*/
+        for (int i = 0; i < horses.size(); i++) {
+            if (horses.get(i).isFinished()) {
+                countFinished++;
+            }
+            else if (!horses.get(i).isFinished()) {
+                System.out.println("Waiting for " + horses.get(i).getName());
+                horses.get(i).join();
+            }
+        }
+        return countFinished;
+    }
+
+    public static class Horse extends Thread {
+
+        public Horse(String name) {
+            super(name);
+        }
+
+        private boolean isFinished;
+
+        public boolean isFinished() {
+            return isFinished;
+        }
+
+        public void run() {
+            String s = "";
+            for (int i = 0; i < 1001; i++) {   //delay
+                s += new String("" + i);
+                if (i == 1000) {
+                    s = " has finished the race!";
+                    System.out.println(getName() + s);
+                    isFinished = true;
+                }
+            }
+        }
+    }
+
+    public static List<Horse> prepareHorsesAndStart() {
+        List<Horse> horses = new ArrayList<Horse>(countHorses);
+        String number;
+        for (int i = 1; i < countHorses + 1; i++) {
+            number = i < 10 ? ("0" + i) : "" + i;
+            horses.add(new Horse("Horse_" + number));
+        }
+
+        for (int i = 0; i < countHorses; i++) {
+            horses.get(i).start();
+        }
+        return horses;
+    }
+}
