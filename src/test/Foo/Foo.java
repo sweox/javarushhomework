d18a512852a5075adab3867f1cd59303404ecdd8
Belshina <Belshina@Asutp-pc>
2016-03-06 04:50:20 +0300

    Head Fist Java 2ns Edition

---
+++ src/test/Foo/Foo.java
@@ -0,0 +1,22 @@
+package test.Foo;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Belshina on 06.03.16.
+ */
+public class Foo
+{
+     int x = 12;
+
+    public  void go(final int x) {
+        System.out.println(x);
+    }
+    double r1 = Math.random();
+    int r2 = (int) (r1 * 5);
+
+    int x = Math.round(6.8f);
+    long v = Math.round(6.0);
+
+    ArrayList<Integer> listOfNumber = new ArrayList<Integer>();
+}
