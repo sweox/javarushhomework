2657b5303d4b68bc55c8a0e7983de9be8e67adb0
Admin <Admin@4530s>
2016-03-14 01:37:52 +0300

    HFJ

---
+++ src/test/test/Clss.java
@@ -0,0 +1,36 @@
+package test.test;
+
+/**
+ * Created by Admin on 13.03.2016.
+ */
+public class Clss
+{
+    public static class One {
+        static int i;
+        public int h;
+        String str = "jj";
+        public static void setOne(int i) {
+            One o = new One();
+            o.Incr(i);
+        }
+        public String getOne() {
+            return str;
+        }
+        public int Incr(int j) {
+            return j++;
+        }
+    }
+
+    public static class Twu {
+        int g;
+    }
+
+    public static void main(String[] args)
+    {
+        One one = new One();
+        Twu twu = new Twu();
+        one.Incr(twu.g);
+        One.setOne(twu.g);
+
+    }
+}
