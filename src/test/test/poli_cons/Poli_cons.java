a697142f288f252097338ab38eab24b93ab339dc
BELSHINA <BELSHINA@BELSHINA-PC>
2016-04-05 01:49:00 +0300

    Poli_cons

---
+++ src/test/test/poli_cons/Poli_cons.java
@@ -0,0 +1,28 @@
+package test.test.poli_cons;
+
+/**
+ * Created by BELSHINA on 05.04.2016.
+ */
+public class Poli_cons
+{
+    class A {
+        String str = "ab";
+        A() {
+            printLength();
+        }
+        private void printLength() {
+            System.out.println(str.length());
+        }
+    }
+    class B extends A {
+        String str = "abc";
+        void printLength() {
+            System.out.println(str.length());
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        new Poli_cons().new B();
+    }
+}
