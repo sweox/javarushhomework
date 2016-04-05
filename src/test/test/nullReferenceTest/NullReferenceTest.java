b93e7acf8829fa1e8295ea9e126d16d293ba4b4a
BELSHINA <BELSHINA@BELSHINA-PC>
2016-04-05 07:07:14 +0300

    NullReferenceTest

---
+++ src/test/test/nullReferenceTest/NullReferenceTest.java
@@ -0,0 +1,12 @@
+package test.test.nullReferenceTest;
+
+/**
+ * Created by BELSHINA on 05.04.2016.
+ */
+public class NullReferenceTest {
+    static String str = "Hello world";
+    public static void main(String[] args) {
+        NullReferenceTest nullReferenceTest = null;
+        System.out.println(nullReferenceTest.str);
+    }
+}
