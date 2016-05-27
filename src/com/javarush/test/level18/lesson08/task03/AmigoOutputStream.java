50cc0235c52b1e539b495593f6b8242632eee87c
BELSHINA <BELSHINA@BELSHINA-PC>
2016-05-27 13:48:18 +0300

    com.javarush.test.level18.lesson08.task03;
    /* AmigoOutputStream
    1 ???????? ????? AmigoOutputStream ???, ????? ?? ???? Wrapper-?? ??? ?????? FileOutputStream. ??????????? ????????????.
    2 ??? ?????? ?????? close() ?????? ??????????? ????????? ?????????????????? ????????:
    2.1 ??????? ????? flush()
    2.2 ???????? ????????? ????? [JavaRush ? 2012-2013 All rights reserved.], ??????????? ????? getBytes()
    2.3 ??????? ????? ??????? close()
    */

--- src/com/javarush/test/level18/lesson08/task03/AmigoOutputStream.java
+++ src/com/javarush/test/level18/lesson08/task03/AmigoOutputStream.java
@@ -53,3 +53,17 @@ public class AmigoOutputStream  extends FileOutputStream {
 
 }
 
+/*
+
+public class AmigoOutputStream {
+    public static String fileName = "C:/tmp/result.txt";
+
+    public static void main(String[] args) throws FileNotFoundException {
+        new AmigoOutputStream(new FileOutputStream(fileName));
+    }
+
+}
+}
+*/
+
+
