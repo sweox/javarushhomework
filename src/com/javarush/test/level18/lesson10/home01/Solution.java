4ebfc63e7f11d9aed61d9b29c7a71b646525ed63
Admin <Admin@4530s>
2016-05-28 22:07:39 +0300

    com.javarush.test.level18.lesson10.home01;
    /* ?????????? ?????
    ? ????? main ?????? ?????????? ???????? ??? ?????.
    ????????? ?????????? ???? ??????????? ????????, ??????? ???? ? ???? ?????.
    ??????? ?? ????? ????? (?????????? ????)
    ??????? ??????. ?? ???????????? try-with-resources
    */

--- src/com/javarush/test/level18/lesson10/home01/Solution.java
+++ src/com/javarush/test/level18/lesson10/home01/Solution.java
@@ -10,14 +10,39 @@ import java.io.IOException;
 import java.io.FileInputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Solution {
+    //static int countChar;
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        FileInputStream inputStream = new FileInputStream(reader.readLine());
+
+        FileInputStream inputStream = new FileInputStream(args[0]);
+        ArrayList<Character> alphabet = new ArrayList<>();
+
+        int countChar = 0;
+
+        for(char j = 'A'; j <= 'Z'; j++) {
+            alphabet.add(j);
+        }
+        for (char j = 'a'; j <= 'z'; j++) {
+            alphabet.add(j);
+        }
+
 
         while(inputStream.available()>0) {
-            System.out.print(inputStream.read() + " ");
+            if (alphabet.contains((char)inputStream.read()))
+                countChar = countChar + 1;
         }
+        inputStream.close();
+        System.out.println(countChar);
     }
 }
+
+/*
+
+public class Solution {
+    //static int countChar;
+    public static void main(String[] args) throws IOException {
+    }
+}
+*/
