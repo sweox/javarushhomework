83038d9de9a4430ae66f7914eb92080e3bd0c564
Admin <Admin@4530s>
2016-05-30 23:14:21 +0300

    com.javarush.test.level18.lesson10.home03;
    /* ??? ? ?????
    ??????? ? ??????? 3 ????? ?????
    ???????? ? ?????? ???? ??????????? ??????? ?????, ? ????? ???????? ? ?????? ???? ?????????? ???????? ?????
    ??????? ??????. ?? ???????????? try-with-resources
    */

--- src/com/javarush/test/level18/lesson10/home03/Solution.java
+++ src/com/javarush/test/level18/lesson10/home03/Solution.java
@@ -10,12 +10,11 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-//import java.util.Arrays;
+
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        FileInputStream inputStream1;
         FileInputStream inputStream2;
         FileInputStream inputStream3;
         FileOutputStream outputStream;
@@ -23,19 +22,26 @@ public class Solution {
         String fileName1 = reader.readLine();
         String fileName2 = reader.readLine();
         String fileName3 = reader.readLine();
-        inputStream1 = new FileInputStream(fileName1);
+
         inputStream2 = new FileInputStream(fileName2);
         inputStream3 = new FileInputStream(fileName3);
-
         outputStream = new FileOutputStream(fileName1);
-        while(inputStream2.available() > 0) {
-            byte[] dataByte = new byte[inputStream2.available()];
-            int data = inputStream2.read(dataByte);
-            outputStream.write(data);
-        }
-        while(inputStream1.available() > 0) {
-            byte[] dataByte = new byte[inputStream1.available() + inputStream3.available()];
-        }
+
+
+        byte[] dataByte2 = new byte[inputStream2.available()];
+        int count2 = inputStream2.read(dataByte2);
+        outputStream.write(dataByte2, 0, count2);
+
+
+
+        byte[] dataByte3 = new byte[inputStream3.available()];
+        int count3 = inputStream3.read(dataByte3);
+        outputStream.write(dataByte3, 0, count3);
+
+        reader.close();
+        inputStream2.close();
+        inputStream3.close();
+        outputStream.close();
 
     }
 }
