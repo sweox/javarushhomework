3fdb51b52015bba21d12792e72deed693fdb71af
Admin <Admin@4530s>
2016-05-30 23:28:02 +0300

    com.javarush.test.level18.lesson10.home03;
    /* ??? ? ?????
    ??????? ? ??????? 3 ????? ?????
    ???????? ? ?????? ???? ??????????? ??????? ?????, ? ????? ???????? ? ?????? ???? ?????????? ???????? ?????
    ??????? ??????. ?? ???????????? try-with-resources
    */

--- src/com/javarush/test/level18/lesson10/home03/Solution.java
+++ src/com/javarush/test/level18/lesson10/home03/Solution.java
@@ -5,12 +5,46 @@ package com.javarush.test.level18.lesson10.home03;
 Записать в первый файл содержимого второго файла, а потом дописать в первый файл содержимое третьего файла
 Закрыть потоки. Не использовать try-with-resources
 */
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String file1 = br.readLine();
+        String file2 = br.readLine();
+        String file3 = br.readLine();
+        br.close();
+
+        FileOutputStream fileOutputStream = new FileOutputStream(file1);
+        FileInputStream fileInputStream2 = new FileInputStream(file2);
+        FileInputStream fileInputStream3 = new FileInputStream(file3);
+
+        byte[] buffer2 = new byte[fileInputStream2.available()];
+        fileInputStream2.read(buffer2);
+        fileInputStream2.close();
+
+        byte[] buffer3 = new byte[fileInputStream3.available()];
+        fileInputStream3.read(buffer3);
+
+        fileOutputStream.write(buffer2);
+        fileOutputStream.write(buffer3);
+
+        fileOutputStream.close();
+        fileInputStream2.close();
+        fileInputStream3.close();
+
+
+    }
+}
+//Мой вариант почему-то не работал!!!!
+/*
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-
 
 public class Solution {
     public static void main(String[] args) throws IOException {
@@ -45,3 +79,4 @@ public class Solution {
 
     }
 }
+*/
