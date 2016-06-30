d290472a64bfcb773121467622e3ea88de4add96
Admin <Admin@4530s>
2016-06-30 21:38:28 +0300

    com.javarush.test.level19.lesson08.task03;
    /* ??????? ?????? ?????
    ? ?????? main ????????? ?????? System.out ?????????? ???? ?????-???????? ?? ???????? ? ???????
    ???? ?????-??????? ?????? ???????? ?????? ?????
    ???????? ??????? ????? printSomething(), ?????????????? testString
    ??????? ?????????? System.out ?????????????? ?????
    ??????? ???????????????? ?????? ? ???????.
    
    ?????? ??????:
    12345678
    */

---
+++ src/com/javarush/test/level19/lesson08/task03/Solution.java
@@ -0,0 +1,63 @@
+package com.javarush.test.level19.lesson08.task03;
+
+/* Выводим только цифры
+В методе main подмените объект System.out написанной вами ридер-оберткой по аналогии с лекцией
+Ваша ридер-обертка должна выводить только цифры
+Вызовите готовый метод printSomething(), воспользуйтесь testString
+Верните переменной System.out первоначальный поток
+Вывести модифицированную строку в консоль.
+
+Пример вывода:
+12345678
+*/
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream outConsole = System.out;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(baos);
+        System.setOut(stream);
+
+        testString.printSomething();
+
+        byte[] strMas = baos.toByteArray();
+
+        System.setOut(outConsole);
+
+        for(byte b : strMas) {
+            if(Character.isDigit(b)) {
+                System.out.print((char)b);
+            }
+        }
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's 1 a 23 text 4 f5-6or7 tes8ting");
+        }
+    }
+}
+/*
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's 1 a 23 text 4 f5-6or7 tes8ting");
+        }
+    }
+}
+*/
