9723eb44772cd5e11ae9f6b9ce66d1e484fd99e4
Admin <Admin@4530s>
2016-07-06 01:49:34 +0300

    com.javarush.test.level19.lesson10.home08;
    /* ???????????
    1 ??????? ? ??????? ??? ?????.
    2 ??? ?????? ?????? ? ?????:
    2.1 ??????????? ??? ??????? ? ???????? ???????
    2.2 ??????? ?? ?????
    3 ??????? ??????. ?? ???????????? try-with-resources
    
    ?????? ???? ???????? ?????:
    ? - ???????????.
    ?????
    
    ?????? ??????????:
    .??????????? - ?
    ?????
    */

---
+++ src/com/javarush/test/level19/lesson10/home08/Solution.java
@@ -0,0 +1,66 @@
+package com.javarush.test.level19.lesson10.home08;
+
+/* Перевертыши
+1 Считать с консоли имя файла.
+2 Для каждой строки в файле:
+2.1 переставить все символы в обратном порядке
+2.2 вывести на экран
+3 Закрыть потоки. Не использовать try-with-resources
+
+Пример тела входного файла:
+я - программист.
+Амиго
+
+Пример результата:
+.тсиммаргорп - я
+огимА
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader readLine = new BufferedReader(new InputStreamReader(System.in));
+        String pathFileName = readLine.readLine();
+        readLine.close();
+        BufferedReader readerFromFile = new BufferedReader(new FileReader(pathFileName));
+        while(readerFromFile.ready()) {
+            String str = readerFromFile.readLine();
+            //revert(str);
+            revertRec(str.toCharArray(), 0);
+            System.out.println(str);
+
+        }
+        readerFromFile.close();
+    }
+
+    public static void revert(String str) {
+        StringBuilder stringBuilder = new StringBuilder(str);
+        System.out.println(stringBuilder.reverse());
+    }
+
+    public static void revertRec(char[] chArr, int off) {
+        if(off < chArr.length/2)
+        {
+            char tmp = chArr[off];
+            chArr[off] = chArr[chArr.length - 1 - off];
+            chArr[chArr.length - 1 - off] = tmp;
+
+            off++;
+            revertRec(chArr, off);
+        }
+        //System.out.println(chArr);
+
+    }
+}
+
+/*
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
+*/
