06f850118b9623eea4342e370c3f540ba915071a
BELSHINA <BELSHINA@BELSHINA-PC>
2016-07-06 16:37:42 +0300

    com.javarush.test.level19.lesson10.home09;
    /* ??????????? ???????
    ? ?????? main ????????? ?????? System.out ?????????? ???? ??????-????????
    ???? ??????-??????? ?????? ???????? ?? ??????? ??????????? ??????? ????? ??????? ??????? println-?
    ???????? ??????? ????? printSomething(), ?????????????? testString
    ??????? ?????????? System.out ?????????????? ?????
    
    ????????? ?????: "JavaRush - ????? Java ??????"
    
    ?????? ??????:
    first
    second
    JavaRush - ????? Java ??????
    third
    fourth
    JavaRush - ????? Java ??????
    fifth
    */

---
+++ src/com/javarush/test/level19/lesson10/home09/Solution.java
@@ -0,0 +1,75 @@
+package com.javarush.test.level19.lesson10.home09;
+
+/* Контекстная реклама
+В методе main подмените объект System.out написанной вами реадер-оберткой
+Ваша реадер-обертка должна выводить на консоль контекстную рекламу после каждого второго println-а
+Вызовите готовый метод printSomething(), воспользуйтесь testString
+Верните переменной System.out первоначальный поток
+
+Рекламный текст: "JavaRush - курсы Java онлайн"
+
+Пример вывода:
+first
+second
+JavaRush - курсы Java онлайн
+third
+fourth
+JavaRush - курсы Java онлайн
+fifth
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream consoleOut = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+        testString.printSomething();
+
+        String str = outputStream.toString();
+
+        System.setOut(consoleOut);
+
+        String [] strArr = str.split("\n");
+        for(int i = 0; i < strArr.length; i++) {
+            System.out.println(strArr[i]);
+            if((i+1)%2 == 0) {
+                System.out.println("JavaRush - курсы Java онлайн");
+            }
+        }
+        stream.close();
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("first");
+            System.out.println("second");
+            System.out.println("third");
+            System.out.println("fourth");
+            System.out.println("fifth");
+        }
+    }
+}
+
+/*
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("first");
+            System.out.println("second");
+            System.out.println("third");
+            System.out.println("fourth");
+            System.out.println("fifth");
+        }
+    }
+}
+*/
