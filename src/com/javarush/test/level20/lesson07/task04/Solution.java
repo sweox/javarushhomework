8cfa609f756b2d6e6857c5e310b71e4a35fd6655
BELSHINA <BELSHINA@BELSHINA-PC>
2016-08-01 14:26:16 +0300

    level20.lesson07.task04;
    /* Serializable Solution
    ???????????? ????? Solution.
    ?????????, ????? ???? ?? ????? ?????????????, ???????? ???????? ???? ? transient.
    ?????? ?????? ?????? ????????? ?????????? ???????? ??????.
    ????? main ?? ????????? ? ????????????.
    ???????? ??? ???????? ?????????????? ? ?????? main:
    1) ??????? ????, ??????? ????? ?? ?????? (input stream) ? ?? ??????(output stream)
    2) ??????? ????????? ?????? Solution - savedObject
    3) ???????? ? ????? ?? ?????? savedObject (?????????, ??? ??? ??? ????????????? ????)
    4) ??????? ?????? ????????? ?????? Solution ? ?????? ??????????
    5) ????????? ?? ?????? ?? ?????? ?????? - loadedObject
    6) ?????????, ??? savedObject.string ????? loadedObject.string
    7) ?????????? ??????????
    */

---
+++ src/com/javarush/test/level20/lesson07/task04/Solution.java
@@ -0,0 +1,85 @@
+package com.javarush.test.level20.lesson07.task04;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/* Serializable Solution
+Сериализуйте класс Solution.
+Подумайте, какие поля не нужно сериализовать, пометить ненужные поля — transient.
+Объект всегда должен содержать актуальные итоговые данные.
+Метод main не участвует в тестировании.
+Написать код проверки самостоятельно в методе main:
+1) создать файл, открыть поток на чтение (input stream) и на запись(output stream)
+2) создать экземпляр класса Solution - savedObject
+3) записать в поток на запись savedObject (убедитесь, что они там действительно есть)
+4) создать другой экземпляр класса Solution с другим параметром
+5) загрузить из потока на чтение объект - loadedObject
+6) проверить, что savedObject.string равна loadedObject.string
+7) обработать исключения
+*/
+public class Solution implements Serializable {
+    public static void main(String[] args) {
+        System.out.println(new Solution(4));
+        try {
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("c:/4.txt"));
+            ObjectInputStream in = new ObjectInputStream(new FileInputStream("c:/4.txt"));
+            Solution savedObject = new Solution(32);
+            out.writeObject(savedObject);
+            out.close();
+            Solution loadedObject = new Solution(27);
+            loadedObject = (Solution)in.readObject();
+            System.out.println(savedObject);
+            System.out.println(loadedObject);
+        }
+        catch(FileNotFoundException e) {e.printStackTrace();}
+        catch(ClassNotFoundException e) {e.printStackTrace();}
+        catch(IOException e) {e.printStackTrace();}
+    }
+
+    transient private final String pattern = "dd MMMM yyyy, EEEE";
+    transient private Date currentDate;
+    transient private int temperature;
+    String string;
+
+    public Solution(int temperature) {
+        this.currentDate = new Date();
+        this.temperature = temperature;
+
+        string = "Today is %s, and current temperature is %s C";
+        SimpleDateFormat format = new SimpleDateFormat(pattern);
+        this.string = String.format(string, format.format(currentDate), temperature);
+    }
+
+    @Override
+    public String toString() {
+        return this.string;
+    }
+}
+/*
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution(4));
+    }
+
+    private final String pattern = "dd MMMM yyyy, EEEE";
+    private Date currentDate;
+    private int temperature;
+    String string;
+
+    public Solution(int temperature) {
+        this.currentDate = new Date();
+        this.temperature = temperature;
+
+        string = "Today is %s, and current temperature is %s C";
+        SimpleDateFormat format = new SimpleDateFormat(pattern);
+        this.string = String.format(string, format.format(currentDate), temperature);
+    }
+
+    @Override
+    public String toString() {
+        return this.string;
+    }
+}
+*/
