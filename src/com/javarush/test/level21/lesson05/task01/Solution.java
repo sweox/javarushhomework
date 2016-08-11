b3b87cb9cccc420b57bd10f35904cb5bd95bb94e
BELSHINA <BELSHINA@BELSHINA-PC>
2016-08-12 01:20:49 +0300

    level21.lesson05.task01;
    /* Equals and HashCode
    ? ?????? Solution ????????? ???? ??????? equals/hashCode ? ???????????? ? ????????? ?????????? ???? ???????.
    ????? main ?? ????????? ? ????????????.
    */

---
+++ src/com/javarush/test/level21/lesson05/task01/Solution.java
@@ -0,0 +1,65 @@
+package com.javarush.test.level21.lesson05.task01;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* Equals and HashCode
+В классе Solution исправить пару методов equals/hashCode в соответствии с правилами реализации этих методов.
+Метод main не участвует в тестировании.
+*/
+
+public class Solution {
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public boolean equals(Object obj) {
+        if(obj == null)
+            return false;
+        if(this == obj)
+            return true;
+        if(obj.getClass() != this.getClass())
+            return false;
+        Solution solution = (Solution)obj;
+        return ((first == solution.first) || first != null && first.equals(solution.first == null ? false : solution.first))
+                && ((last == solution.last) || last != null && last.equals(solution.last == null ? false : solution.last));
+    }
+
+    public int hashCode() {
+        return 31 * (first == null ? 0 : first.hashCode())
+                + (last != null ? last.hashCode() : 0);
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Donald", "Duck"));
+        System.out.println(s.contains(new Solution("Donald", "Duck")));
+    }
+}
+/*
+
+public class Solution {
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public boolean equals(Solution n) {
+        return n.first.equals(first) && n.last.equals(last);
+    }
+
+    public int hashCode() {
+        return 31 * first.hashCode() + last.hashCode();
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Donald", "Duck"));
+        System.out.println(s.contains(new Solution("Donald", "Duck")));
+    }
+}*/
