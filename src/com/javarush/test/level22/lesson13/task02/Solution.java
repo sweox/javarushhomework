63432fa16e456b1fc63eb1a531c71581db15f0ac
Admin <Admin@4530s>
2016-09-15 00:46:00 +0200

    level22.lesson13.task02;
    /* ????? ?????????
    ? ????? main ?????? ?????????? ???????? ??? ?????, ???? ???????? ? ????????? Windows-1251.
    ? ????? main ?????? ?????????? ???????? ??? ?????, ? ??????? ?????????? ???????? ?????????? ??????? ????? ? ????????? UTF-8.
    */

---
+++ src/com/javarush/test/level22/lesson13/task02/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.test.level22.lesson13.task02;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/* Смена кодировки
+В метод main первым параметром приходит имя файла, тело которого в кодировке Windows-1251.
+В метод main вторым параметром приходит имя файла, в который необходимо записать содержимое первого файла в кодировке UTF-8.
+*/
+
+public class Solution {
+    static String win1251TestString = "РќР°СЂСѓС€РµРЅРёРµ РєРѕРґРёСЂРѕРІРєРё РєРѕРЅСЃРѕР»Рё?"; //only for your testing
+
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream in = new FileInputStream(args[0]);
+        byte[] bytes = new byte[in.available()];
+        in.read(bytes);
+        in.close();
+
+        String strUTF = new String(bytes);
+        byte[] bCP1251 = strUTF.getBytes("Windows-1251");
+        FileOutputStream out = new FileOutputStream(args[1]);
+        out.write(bCP1251);
+        out.close();
+
+    }
+}
+/*
+
+public class Solution {
+    static String win1251TestString = "РќР°СЂСѓС€РµРЅРёРµ РєРѕРґРёСЂРѕРІРєРё РєРѕРЅСЃРѕР»Рё?"; //only for your testing
+
+    public static void main(String[] args) throws IOException {
+    }
+}
+*/
