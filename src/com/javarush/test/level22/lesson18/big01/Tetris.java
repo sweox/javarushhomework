ae679f8149227cd0b2d314c246cfc2d6a96c6aaa
BELSHINA <BELSHINA@BELSHINA-PC>
2016-09-21 15:42:16 +0300

    test.level22.lesson18.big01 ??????? 3

--- src/com/javarush/test/level22/lesson18/big01/Tetris.java
+++ src/com/javarush/test/level22/lesson18/big01/Tetris.java
@@ -4,6 +4,14 @@ package com.javarush.test.level22.lesson18.big01;
  * Created by BELSHINA on 21.09.2016.
  */
 public class Tetris {
+    Field field;
+    Figure figure;
+    public Field getField() {
+        return this.field;
+    }
+    public Figure getFigure() {
+        return this.figure;
+    }
     public static void main(String[] args) {
 
     }
