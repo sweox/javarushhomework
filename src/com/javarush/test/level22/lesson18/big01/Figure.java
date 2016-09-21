a52e051d5e2f4fefd7ff39512f60387839798c0e
BELSHINA <BELSHINA@BELSHINA-PC>
2016-09-21 17:34:43 +0300

    test.level22.lesson18.big01 ??????? 9

--- src/com/javarush/test/level22/lesson18/big01/Figure.java
+++ src/com/javarush/test/level22/lesson18/big01/Figure.java
@@ -23,4 +23,13 @@ public class Figure {
         this.y = y;
         this.matrix = matrix;
     }
+
+    public void left() {}
+    public void right() {}
+    public void down() {}
+    public void up() {}
+    public void downMaximum() {}
+    public void rotate() {}
+    public boolean isCurrentPositionAvailable() {return true;}
+    public void landed() {}
 }
