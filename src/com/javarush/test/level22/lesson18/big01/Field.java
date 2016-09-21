8d68c934021ee694fa3d8554f7ce5e7931000304
BELSHINA <BELSHINA@BELSHINA-PC>
2016-09-21 17:10:44 +0300

    test.level22.lesson18.big01 ??????? 6

--- src/com/javarush/test/level22/lesson18/big01/Field.java
+++ src/com/javarush/test/level22/lesson18/big01/Field.java
@@ -4,4 +4,23 @@ package com.javarush.test.level22.lesson18.big01;
  * Created by BELSHINA on 21.09.2016.
  */
 public class Field {
+    public int width;
+    public int height;
+    public int[][] matrix;
+
+    public int getWidth() {
+        return this.width;
+    }
+    public int getHeight() {
+        return this.height;
+    }
+    public int[][] getMatrix() {
+        return this.matrix;
+    }
+
+    public Field(int width, int height) {
+        this.width = width;
+        this.height = height;
+        matrix = new int[height][width];
+    }
 }
