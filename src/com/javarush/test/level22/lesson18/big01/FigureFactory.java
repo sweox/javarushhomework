40c963d1e7b94ce578748f4e59c9a23e3bdd4140
BELSHINA <BELSHINA@BELSHINA-PC>
2016-09-21 17:38:42 +0300

    test.level22.lesson18.big01 ??????? 11

---
+++ src/com/javarush/test/level22/lesson18/big01/FigureFactory.java
@@ -0,0 +1,8 @@
+package com.javarush.test.level22.lesson18.big01;
+
+/**
+ * Created by BELSHINA on 21.09.2016.
+ */
+public class FigureFactory {
+    public static Figure createRandomFigure(int x,int y) {return null;}
+}
