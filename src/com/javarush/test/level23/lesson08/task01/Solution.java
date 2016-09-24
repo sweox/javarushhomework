dffd2450d13cc99e54d627d01138af9e596c45da
BELSHINA <BELSHINA@BELSHINA-PC>
2016-09-24 21:31:12 +0300

    level23.lesson08.task01;
    /* ??????? ????????!
    ????? printName ?????? ???????? ???? ??????????? ???, ?.?. "sout"
    ???????? ??????? ?????????.
    */

---
+++ src/com/javarush/test/level23/lesson08/task01/Solution.java
@@ -0,0 +1,72 @@
+package com.javarush.test.level23.lesson08.task01;
+
+/* Напряги извилины!
+Метод printName должен выводить свое собственное имя, т.е. "sout"
+Сделайте минимум изменений.
+*/
+public class Solution {
+    private String name;
+
+    Solution(String name) {
+        this.name = name;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    private void sout() {
+        new Solution("sout") {
+            void printName() {
+                System.out.println(getName());
+            }
+        }.printName();
+    }
+
+//    private void sout() {
+//        class MySolution extends Solution {
+//            MySolution(String name) {
+//                super(name);
+//            }
+//            void printNaem() {
+//                System.out.println(getName()/*super.getName()*/);
+//            }
+//
+//        }
+//        MySolution m = new MySolution("sout");
+//        m.printNaem();
+//    }
+
+
+    public static void main(String[] args) {
+        new Solution("main").sout();
+    }
+}
+
+
+/*
+
+public class Solution {
+    private String name;
+
+    Solution(String name) {
+        this.name = name;
+    }
+
+    private String getName() {
+        return name;
+    }
+
+    private void sout() {
+        new Solution("sout") {
+            void printName() {
+                System.out.println(getName());
+            }
+        }.printName();
+    }
+
+    public static void main(String[] args) {
+        new Solution("main").sout();
+    }
+}
+*/
