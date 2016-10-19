369b8625f48adbff84d4bca8156bd30d79e9f811
BELSHINA <BELSHINA@BELSHINA-PC>
2016-10-19 08:52:37 +0300

    level24.lesson02.home01;
    /* ???????? ?????? ??????????-???????
    1. ???????? ?????????-?????? SelfInterfaceMarker
    2. ???????? ????? SelfInterfaceMarkerImpl, ??????? ????????? SelfInterfaceMarker
    3. ???????? ? SelfInterfaceMarkerImpl ??????? 2 ????? ????????? public ?????? (?????????? public ??????????? ?????? ??????????????)
    4. ???????? ?????????? UnsupportedInterfaceMarkerException
    5. ? ?????? testClass ?????? Util: ???? ???????? == null, ?? ????????? UnsupportedInterfaceMarkerException
    */

---
+++ src/com/javarush/test/level24/lesson02/home01/SelfInterfaceMarker.java
@@ -0,0 +1,7 @@
+package com.javarush.test.level24.lesson02.home01;
+
+/**
+ * Created by BELSHINA on 19.10.2016.
+ */
+public interface SelfInterfaceMarker {
+}
---
+++ src/com/javarush/test/level24/lesson02/home01/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,10 @@
+package com.javarush.test.level24.lesson02.home01;
+
+/**
+ * Created by BELSHINA on 19.10.2016.
+ */
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+    public SelfInterfaceMarkerImpl() {}
+    public void method1() {}
+    public void method2() {}
+}
