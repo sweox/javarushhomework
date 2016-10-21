859576e7863b8859f064dbb658f2f79c1339dfed
BELSHINA <BELSHINA@BELSHINA-PC>
2016-10-21 10:59:39 +0300

    level24.lesson06.home01;
    /* ?????????? ?????????? ????????? ????????? ?????
    ? ?????? Cat ?????????? ?????? ?????? toSayable, ??????? ??????? ? ?????????.
    */

---
+++ src/com/javarush/test/level24/lesson06/home01/Cat.java
@@ -0,0 +1,84 @@
+package com.javarush.test.level24.lesson06.home01;
+
+/*
+В работе вам иногда будет нужно закастить класс к какому-нибудь интерфейсу (тут Sayable),
+который не реализован в текущем классе
+ */
+
+public class Cat implements Pet {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Это - механизм адаптирования к другому интерфейсу - Sayable
+     * Внутри метода toSayable создайте class CatPet, реализующий интерфейс Sayable
+     * Логика метода say:
+     * Если i <= 0, то вывести на экран, что кот спит. Пример, "Васька спит."'
+     * Иначе вывести фразу: "имя_кота говорит мяу!". Пример для i=3, "Васька говорит мяяяу!"
+     * <p/>
+     * <b>Пример вывода:</b>
+     * Мурзик спит.
+     * Васька говорит мяяу!
+     * Кошка говорит мяяяяяу!
+     * Мыша пищит.
+     * Томас говорит мяу!
+     * <p/>
+     * @param i количество букв 'я' в слове мяу
+     * @return экземпляр класса CatPet
+     */
+    public Sayable toSayable(final int i) {
+        String na = "";
+        for(int j = 1; j <= i; j++) {
+            na = na + "я";
+        }
+        final String naFin = na;
+        class CatPet implements Sayable {
+            public String say() {
+                if(i <= 0)
+                    return name + " спит.";
+                else
+                    return name + " говорит м" + naFin + "у!";
+            }
+        }
+        return new CatPet();
+    }
+}
+
+/*
+
+public class Cat implements Pet {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    */
+/**
+     * Это - механизм адаптирования к другому интерфейсу - Sayable
+     * Внутри метода toSayable создайте class CatPet, реализующий интерфейс Sayable
+     * Логика метода say:
+     * Если i <= 0, то вывести на экран, что кот спит. Пример, "Васька спит."'
+     * Иначе вывести фразу: "имя_кота говорит мяу!". Пример для i=3, "Васька говорит мяяяу!"
+     * <p/>
+     * <b>Пример вывода:</b>
+     * Мурзик спит.
+     * Васька говорит мяяу!
+     * Кошка говорит мяяяяяу!
+     * Мыша пищит.
+     * Томас говорит мяу!
+     * <p/>
+     * @param i количество букв 'я' в слове мяу
+     * @return экземпляр класса CatPet
+     *//*
+
+    public Sayable toSayable(final int i) {
+        return null;
+    }
+}
+*/
+
+
