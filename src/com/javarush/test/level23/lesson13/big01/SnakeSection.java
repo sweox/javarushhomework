da9f2f5c53da45dfed7c7aeb6a0c92f7bd6459cc
BELSHINA <BELSHINA@BELSHINA-PC>
2016-10-23 16:46:02 +0300

    level23.lesson13.big01 ??????? 6

--- src/com/javarush/test/level23/lesson13/big01/SnakeSection.java
+++ src/com/javarush/test/level23/lesson13/big01/SnakeSection.java
@@ -4,4 +4,16 @@ package com.javarush.test.level23.lesson13.big01;
  * Created by BELSHINA on 23.10.2016.
  */
 public class SnakeSection {
+    public int x;
+    public int y;
+    public SnakeSection(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
 }
