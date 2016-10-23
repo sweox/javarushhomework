6376ad222d0e6e8c226895d58755366a4ae211bc
BELSHINA <BELSHINA@BELSHINA-PC>
2016-10-23 18:03:16 +0300

    level23.lesson13.big01 ??????? 7

--- src/com/javarush/test/level23/lesson13/big01/Snake.java
+++ src/com/javarush/test/level23/lesson13/big01/Snake.java
@@ -1,7 +1,25 @@
 package com.javarush.test.level23.lesson13.big01;
 
+import java.util.ArrayList;
+
 /**
  * Created by BELSHINA on 23.10.2016.
  */
 public class Snake {
+    private ArrayList<SnakeSection> sections = new ArrayList<>();
+    private boolean isAlive;
+    private SnakeDirection direction;
+
+    public ArrayList<SnakeSection> getSections() {
+        return sections;
+    }
+    public boolean isAlive() {
+        return isAlive;
+    }
+    public void setDirection(SnakeDirection direction) {
+        this.direction = direction;
+    }
+    public SnakeDirection getDirection() {
+        return direction;
+    }
 }
