5a28c2e5ae17cc4ed778eb24f527f1e2dc7c2d1a
BELSHINA <BELSHINA@BELSHINA-PC>
2016-11-24 20:40:48 +0300

    level24.lesson14.big01 ??????? 12

--- src/com/javarush/test/level24/lesson14/big01/Brick.java
+++ src/com/javarush/test/level24/lesson14/big01/Brick.java
@@ -7,6 +7,10 @@ public class Brick extends BaseObject {
     public Brick(double x, double y, double radius) {
         super(x, y, radius);
     }
+    public Brick(double x, double y)
+    {
+        super(x,y,3);
+    }
 
     public void draw(Canvas canvas) {
 
