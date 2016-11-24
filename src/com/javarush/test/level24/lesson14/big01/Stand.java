3d0b3925520757a9bcab6528e072e61d1ec693a8
BELSHINA <BELSHINA@BELSHINA-PC>
2016-11-24 22:21:28 +0300

    level24.lesson14.big01 ??????? 16

--- src/com/javarush/test/level24/lesson14/big01/Stand.java
+++ src/com/javarush/test/level24/lesson14/big01/Stand.java
@@ -4,8 +4,23 @@ package com.javarush.test.level24.lesson14.big01;
  * Created by BELSHINA on 24.11.2016.
  */
 public class Stand extends BaseObject {
+    private double speed;
+    private double direction;
+
+    public double getSpeed() {
+        return speed;
+    }
+    public double getDirection() {
+        return direction;
+    }
+
      public Stand(double x, double y, double radius) {
          super(x, y, radius);
+     }
+     public Stand(double x, double y) {
+         super(x, y, 3);
+         this.speed = 1;
+         this.direction = 0;
      }
 
      public void draw(Canvas canvas) {}
