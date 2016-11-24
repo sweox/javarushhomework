53b6f8a736bb24e162a107208eebeea51ecf5fa1
BELSHINA <BELSHINA@BELSHINA-PC>
2016-11-24 16:31:31 +0300

    level24.lesson14.big01 ??????? 3

--- src/com/javarush/test/level24/lesson14/big01/Arcanoid.java
+++ src/com/javarush/test/level24/lesson14/big01/Arcanoid.java
@@ -4,6 +4,24 @@ package com.javarush.test.level24.lesson14.big01;
  * Created by BELSHINA on 24.11.2016.
  */
 public class Arcanoid {
+    private int width;
+    private int height;
+    public Arcanoid(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    public int getWidth() {
+        return width;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public int getHeight() {
+        return height;
+    }
     public static void main(String[] args) {
 
     }
