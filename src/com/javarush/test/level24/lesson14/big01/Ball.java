23c60ff66dedc5b3d4bf91c867d0256824c8359d
BELSHINA <BELSHINA@BELSHINA-PC>
2016-11-24 21:06:42 +0300

    level24.lesson14.big01 ??????? 13

--- src/com/javarush/test/level24/lesson14/big01/Ball.java
+++ src/com/javarush/test/level24/lesson14/big01/Ball.java
@@ -4,8 +4,33 @@ package com.javarush.test.level24.lesson14.big01;
  * Created by BELSHINA on 24.11.2016.
  */
 public class Ball extends BaseObject {
+    private double speed;
+    private double direction;
+    private double dx;
+    private double dy;
+    private boolean isFrozen;
+
+    public double getSpeed() {
+        return speed;
+    }
+    public double getDirection() {
+        return direction;
+    }
+    public double getDx() {
+        return dx;
+    }
+    public double getDy() {
+        return dy;
+    }
+
     public Ball(double x, double y, double radius) {
         super(x, y, radius);
+    }
+    public Ball(double x, double y, double speed, double direction) {
+        super(x, y, 1);
+        this.speed = speed;
+        this.direction = direction;
+        this.isFrozen = true;
     }
 
     public void draw(Canvas canvas) { }
--- src/com/javarush/test/level24/lesson14/big01/Brick.java
+++ src/com/javarush/test/level24/lesson14/big01/Brick.java
@@ -1,21 +1,39 @@
 package com.javarush.test.level24.lesson14.big01;
 
 /**
- * Created by BELSHINA on 24.11.2016.
+ * Класс для объекта "кирпич".
  */
-public class Brick extends BaseObject {
-    public Brick(double x, double y, double radius) {
-        super(x, y, radius);
-    }
+public class Brick extends BaseObject
+{
+    //картинка для отрисовки
+    private static int[][] matrix = {
+            {0, 0, 0, 0, 0},
+            {0, 1, 1, 1, 0},
+            {0, 1, 1, 1, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+    };
+
     public Brick(double x, double y)
     {
         super(x,y,3);
     }
 
-    public void draw(Canvas canvas) {
-
+    /**
+     * Рисуем себя на холсте
+     */
+    @Override
+    public void draw(Canvas canvas)
+    {
+        canvas.drawMatrix(x - radius + 1, y, matrix, 'H');
     }
-    public void move() {
 
+    /**
+     * Ничего не делаем - кирпич неподвижен
+     */
+    @Override
+    public void move()
+    {
+        //do nothing
     }
 }
