157afeede0dc46d2447f7b199eb7e860420f89e4
BELSHINA <BELSHINA@BELSHINA-PC>
2016-11-24 22:15:17 +0300

    level24.lesson14.big01 ??????? 15

--- src/com/javarush/test/level24/lesson14/big01/Ball.java
+++ src/com/javarush/test/level24/lesson14/big01/Ball.java
@@ -45,4 +45,11 @@ public class Ball extends BaseObject {
     public void start() {
         isFrozen = false;
     }
+    public void setDirection(double direction) {
+        this.direction = direction;
+        double angel = Math.toRadians(direction);
+        dx = Math.cos(angel) * speed;
+        dy = -Math.sin(angel) * speed;
+    }
+    public void checkRebound(int minx, int maxx, int miny, int maxy) {}
 }
