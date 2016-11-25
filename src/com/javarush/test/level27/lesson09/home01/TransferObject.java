821353fc792a8e55170a2dd31188b7301258abb9
BELSHINA <BELSHINA@BELSHINA-PC>
2016-11-25 22:21:18 +0300

    level27.lesson09.home01;
    /* Producer?consumer
    ? ?????? TransferObject ?????????? ????? ??????? wait/notify/notifyAll,
    ????? ?????????? ???????????????? ???????? ? ????????? ???????.
    ????????? ?????:
    ...
    Put: M
    Got: M
    Put: N
    Got: N
    Put: K
    Got: K
    ...
    ??? M, N, K - ?????
    ????? main ?? ????????? ? ????????????
    PS: ?????? ?????????? ???????????? ????? concurrent ?????? ?????? ?????????? wait/notify/notifyAll.
    ?????? ???????? ???????? ?????? ??????????

---
+++ src/com/javarush/test/level27/lesson09/home01/TransferObject.java
@@ -0,0 +1,47 @@
+package com.javarush.test.level27.lesson09.home01;
+
+public class TransferObject {
+    private int value;
+    protected volatile boolean isValuePresent = false; //use this variable
+
+    public synchronized int get() {
+        while(!isValuePresent) {
+            try {
+                this.wait();
+            }
+            catch (InterruptedException e) { }
+        }
+        System.out.println("Got: " + value);
+        isValuePresent = false;
+
+        return value;
+    }
+
+    public synchronized void put(int value) {
+        if(!isValuePresent) {
+            this.value = value;
+            System.out.println("Put: " + value);
+            isValuePresent = true;
+            notifyAll();
+        }
+    }
+}
+
+/*
+
+public class TransferObject {
+    private int value;
+    protected volatile boolean isValuePresent = false; //use this variable
+
+    public synchronized int get() {
+        System.out.println("Got: " + value);
+        return value;
+    }
+
+    public synchronized void put(int value) {
+        this.value = value;
+        System.out.println("Put: " + value);
+    }
+}
+*/
+
