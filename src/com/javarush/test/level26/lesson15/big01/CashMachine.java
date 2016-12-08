fa00744da0f61e0ae23644568d883bb83239b271
Admin <Admin@Admin-X61T>
2016-12-08 23:56:07 +0300

    level26.lesson15.big01 ??????? 1

---
+++ src/com/javarush/test/level26/lesson15/big01/CashMachine.java
@@ -0,0 +1,10 @@
+package com.javarush.test.level26.lesson15.big01;
+
+/**
+ * Created by Admin on 08.12.2016.
+ */
+public class CashMachine {
+    public static void main(String[] args) {
+
+    }
+}
