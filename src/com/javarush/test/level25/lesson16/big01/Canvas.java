ee5bbfea1a84897e4fab96c21f39807a1471ab1c
BELSHINA <BELSHINA@BELSHINA-PC>
2016-12-08 10:27:41 +0300

    level25.lesson16.big01 ??????? 8

--- src/com/javarush/test/level25/lesson16/big01/Canvas.java
+++ src/com/javarush/test/level25/lesson16/big01/Canvas.java
@@ -4,4 +4,24 @@ package com.javarush.test.level25.lesson16.big01;
  * Created by BELSHINA on 08.12.2016.
  */
 public class Canvas {
+    private int width;
+    private int height;
+    char[][] matrix;
+
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public char[][] getMatrix() {
+        return matrix;
+    }
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        matrix = new char[height][width];
+    }
+
 }
