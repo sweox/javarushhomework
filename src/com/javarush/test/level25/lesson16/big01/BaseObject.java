cde8ced2d0919897e3ea0ab700e8e01132370caf
BELSHINA <BELSHINA@BELSHINA-PC>
2016-12-08 09:49:47 +0300

    level25.lesson16.big01 ??????? 6

--- src/com/javarush/test/level25/lesson16/big01/BaseObject.java
+++ src/com/javarush/test/level25/lesson16/big01/BaseObject.java
@@ -4,4 +4,37 @@ package com.javarush.test.level25.lesson16.big01;
  * Created by BELSHINA on 08.12.2016.
  */
 public class BaseObject {
+    private double x;
+    private double y;
+    private double radius;
+    private boolean isAlive;
+
+    public BaseObject(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        isAlive = true;
+    }
+
+    public double getX() {
+        return x;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public double getY() {
+        return y;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public boolean isAlive() {
+        return isAlive;
+    }
 }
--- src/com/javarush/test/level25/lesson16/big01/SpaceShip.java
+++ src/com/javarush/test/level25/lesson16/big01/SpaceShip.java
@@ -4,4 +4,7 @@ package com.javarush.test.level25.lesson16.big01;
  * Created by BELSHINA on 08.12.2016.
  */
 public class SpaceShip extends BaseObject {
+    public SpaceShip(double x, double y, double radius) {
+        super(x, y, radius);
+    }
 }
