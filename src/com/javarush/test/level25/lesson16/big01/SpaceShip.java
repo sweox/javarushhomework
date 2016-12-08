fe47b06e5c54d660f1632c035cde600f4f868a16
BELSHINA <BELSHINA@BELSHINA-PC>
2016-12-08 14:08:46 +0300

    level25.lesson16.big01 ??????? 15

--- src/com/javarush/test/level25/lesson16/big01/SpaceShip.java
+++ src/com/javarush/test/level25/lesson16/big01/SpaceShip.java
@@ -1,26 +1,70 @@
 package com.javarush.test.level25.lesson16.big01;
 
 /**
- * Created by BELSHINA on 08.12.2016.
+ * Класс для космического корабля
  */
-public class SpaceShip extends BaseObject {
-    public SpaceShip(int x, int y) {
+public class SpaceShip extends BaseObject
+{
+    //картинка корабля для отрисовки
+    private static int[][] matrix = {
+            {0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0},
+            {0, 0, 1, 0, 0},
+            {1, 0, 1, 0, 1},
+            {1, 1, 1, 1, 1},
+    };
+
+    //вектор движения (-1 влево,+1 вправо)
+    private double dx = 0;
+
+    public SpaceShip(int x, int y)
+    {
         super(x, y, 3);
     }
 
-    private double dx = 0;
-    public void moveLeft() {
+    /**
+     * Устанавливаем вектор движения влево
+     */
+    public void moveLeft()
+    {
         dx = -1;
     }
-    public void moveRight() {
+
+    /**
+     * Устанавливаем вектор движения вправо
+     */
+    public void moveRight()
+    {
         dx = 1;
     }
-    public void move() {
-        x += dx;
+
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    @Override
+    public void draw(Canvas canvas)
+    {
+        canvas.drawMatrix(x - radius + 1, y, matrix, 'M');
+    }
+
+    /**
+     * Двигаем себя на один ход.
+     * Проверяем столкновение с границами.
+     */
+    @Override
+    public void move()
+    {
+        x = x + dx;
+
         checkBorders(radius, Space.game.getWidth() - radius + 1, 1, Space.game.getHeight() + 1);
     }
-    public void draw() {}
-    public void fire() {
+
+    /**
+     * Стреляем.
+     * Создаем две ракеты: слева и справа от корабля.
+     */
+    public void fire()
+    {
         Space.game.getRockets().add(new Rocket(x - 2, y));
         Space.game.getRockets().add(new Rocket(x + 2, y));
     }
--- src/com/javarush/test/level25/lesson16/big01/Ufo.java
+++ src/com/javarush/test/level25/lesson16/big01/Ufo.java
@@ -4,7 +4,39 @@ package com.javarush.test.level25.lesson16.big01;
  * Created by BELSHINA on 08.12.2016.
  */
 public class Ufo extends BaseObject {
-    public Ufo(double x, double y, double radius) {
-        super(x, y, radius);
+    public Ufo(double x, double y) {
+        super(x, y, 3);
     }
+
+    private static int[][] matrix = {
+            {0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0},
+            {0, 0, 1, 0, 0},
+            {1, 0, 1, 0, 1},
+            {1, 1, 1, 1, 1},
+    };
+
+    public void draw(Canvas canvas)
+    {
+        canvas.drawMatrix(x - radius + 1, y, matrix, 'U');
+    }
+    public void move()
+    {
+        double dx = Math.random() * 2-1;
+        double dy = Math.random() * 2-1;
+
+        x += dx;
+        y += dy;
+
+        checkBorders(radius, Space.game.getWidth() - radius + 1, 1, Space.game.getHeight() / 2);
+
+        int random10 = (int) (Math.random() * 10);
+        if (random10 == 0)
+            fire();
+    }
+    public void fire()
+    {
+        Space.game.getBombs().add(new Bomb(x, y + 3));
+    }
+
 }
