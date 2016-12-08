672b4ea90c86f4e0a5fb0ef4fbd92e0aa6fd7025
BELSHINA <BELSHINA@BELSHINA-PC>
2016-12-08 14:16:39 +0300

    level25.lesson16.big01 ??????? 16

--- src/com/javarush/test/level25/lesson16/big01/Ufo.java
+++ src/com/javarush/test/level25/lesson16/big01/Ufo.java
@@ -1,25 +1,37 @@
 package com.javarush.test.level25.lesson16.big01;
 
 /**
- * Created by BELSHINA on 08.12.2016.
+ * Класс для НЛО
  */
-public class Ufo extends BaseObject {
-    public Ufo(double x, double y) {
-        super(x, y, 3);
-    }
-
+public class Ufo extends BaseObject
+{
+    //картинка для отрисовки
     private static int[][] matrix = {
             {0, 0, 0, 0, 0},
             {0, 0, 1, 0, 0},
-            {0, 0, 1, 0, 0},
-            {1, 0, 1, 0, 1},
             {1, 1, 1, 1, 1},
+            {0, 1, 1, 1, 0},
+            {0, 0, 0, 0, 0},
     };
 
+    public Ufo(double x, double y)
+    {
+        super(x, y, 3);
+    }
+
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    @Override
     public void draw(Canvas canvas)
     {
         canvas.drawMatrix(x - radius + 1, y, matrix, 'U');
     }
+
+    /**
+     * Двигаем себя на один ход в случайном направлении.
+     */
+    @Override
     public void move()
     {
         double dx = Math.random() * 2-1;
@@ -34,9 +46,13 @@ public class Ufo extends BaseObject {
         if (random10 == 0)
             fire();
     }
+
+    /**
+     * Стреляем.
+     * Сбрасываем(создаем) одну бомбу прямо под собой.
+     */
     public void fire()
     {
         Space.game.getBombs().add(new Bomb(x, y + 3));
     }
-
 }
