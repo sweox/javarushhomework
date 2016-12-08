d7c0d8d4eeb6f37eba28abc9c735bd3b9f077730
BELSHINA <BELSHINA@BELSHINA-PC>
2016-12-08 11:00:41 +0300

    level25.lesson16.big01 ??????? 9

--- src/com/javarush/test/level25/lesson16/big01/Canvas.java
+++ src/com/javarush/test/level25/lesson16/big01/Canvas.java
@@ -24,4 +24,16 @@ public class Canvas {
         matrix = new char[height][width];
     }
 
+    public void setPoint(double x, double y, char c) {
+        matrix[(int)Math.round(y)][(int)Math.round(x)] = c;
+    }
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] != 0) {
+                    setPoint(x + j, y + i, c);
+                }
+            }
+        }
+    }
 }
