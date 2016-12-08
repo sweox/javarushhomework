5fa80ce90827162488a19461e115ac65fce362e5
BELSHINA <BELSHINA@BELSHINA-PC>
2016-12-08 11:08:29 +0300

    level25.lesson16.big01 ??????? 10

--- src/com/javarush/test/level25/lesson16/big01/Canvas.java
+++ src/com/javarush/test/level25/lesson16/big01/Canvas.java
@@ -36,4 +36,21 @@ public class Canvas {
             }
         }
     }
+    public void clear() {
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = ' ';
+            }
+        }
+    }
+    public void print() {
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                System.out.println(matrix[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+    }
 }
