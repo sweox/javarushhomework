db08803174c05bee63853616b656108933e56092
Admin <Admin@Admin-X61T>
2016-12-09 00:14:17 +0300

    level26.lesson15.big01 ??????? 2

--- src/com/javarush/test/level26/lesson15/big01/ConsoleHelper.java
+++ src/com/javarush/test/level26/lesson15/big01/ConsoleHelper.java
@@ -1,7 +1,24 @@
 package com.javarush.test.level26.lesson15.big01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by Admin on 08.12.2016.
  */
 public class ConsoleHelper {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+    public static String readString() {
+        String strReturn = "";
+        try {
+            strReturn = reader.readLine();
+        }
+        catch (IOException e) {}
+        return strReturn;
+    }
+
 }
