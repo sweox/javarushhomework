6cba20354854120ab1dd2592c5f64b8c3e852a6b
BELSHINA <BELSHINA@BELSHINA-PC>
2016-12-23 09:06:56 +0300

    level26.lesson10.home02;
    /* ??? ?? ????????, ???????? ??.
    ??????????? ? ConcurrentHashMap
    ? ????????? ????? ???????? ????? Producer, ??????? ?????:
    1. ?????? ?????????? ???????? ?? ??????? ? ????? ?????? ??????? ? 1 ????? [Some text for i] , ?????? "Some text for 1"
    2. ??? ????????????? ?????????? ???????? ? ??????? [[TREAD_NAME] thread was terminated], ?????? "[thread-1] thread was terminated"
    */

---
+++ src/com/javarush/test/level26/lesson10/home02/Producer.java
@@ -0,0 +1,32 @@
+package com.javarush.test.level26.lesson10.home02;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by BELSHINA on 23.12.2016.
+ */
+
+public class Producer implements Runnable {
+    private ConcurrentHashMap<String, String> map;
+
+    public Producer(ConcurrentHashMap<String, String> map) {
+        this.map = map;
+    }
+
+    @Override
+    public void run() {
+        int i = 0;
+        try{
+            while(!Thread.currentThread().isInterrupted()) {
+                map.put(Integer.toString(++i), "Some text for " + i);
+                Thread.sleep(500);
+            }
+
+        }
+        catch(InterruptedException e) {
+            System.out.printf("[%s] thread was terminated", Thread.currentThread().getName());
+        }
+    }
+}
+
+//Этого класса изначально не было
